0f795510077380671f798caf8cd37533933f961a
Michael Netto <mike.netto87@gmail.com>
2022-09-15 20:45:26 -0400

    added variables and debugged to ensure functionality

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ function writePassword() {
 
 }
 
+var characterLength = 8;
+var choiceArray = [];
+var specialCharacterArray = ["!", "$", "%", "&", "*", "?"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"];
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+// console.log(specialCharacterArray);
+// console.log(lowercaseArray);
+// console.log(uppercaseArray);
+// console.log(numberArray);
+
+function generatePassword(){
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
